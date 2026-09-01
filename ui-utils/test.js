6ea6c39f99ef698b8dc279da4148e69e290dd1cf
maor <maorsasson12@gmail.com>
2026-09-01 17:04:30 +0300

    new version of ui-utils

--- ui-utils/test.js
+++ ui-utils/test.js
@@ -2,7 +2,7 @@ import { getGreeting } from "./index.js";
 
 const result = getGreeting("Maor");
 
-if (result !== "Hello Maor, welcome to my DevOps lab!") {
+if (result !== "🚀 Hello Maor! Welcome to DevOps Lab v2.0!") {
     console.error("TEST FAILED");
     process.exit(1);
 }
